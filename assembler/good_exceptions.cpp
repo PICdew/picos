3c4695df8a91258dfdda4ad5e790ae94d92fc6e7
David Coss (hostname: turing) <David.Coss@stjude.org>
2011-05-16 13:10:48 -0500

    Cleared out old simulator code. Could be replaced later...

--- assembler/good_exceptions.cpp
+++
@@ -1,8 +0,0 @@
-#include "good_exceptions.h"
-
-std::ostream& operator<<(std::ostream& theStream,GoodException& ge)
-{
-    theStream << ge.identifier() << "(" << ge.getErrType() << "): " << ge.what() << std::endl;
-    return theStream;
-}
-
