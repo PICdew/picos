4343e187a32d85617adf5d4084c233429d866e00
David Coss <david@davecoss.com>
2011-09-22 13:46:33 -0500

    As PROTOTYPING, sd.h belongs in picos. The IMPLEMENTATION belongs with the device specific code.

---
+++ sd.h
@@ -0,0 +1,33 @@
+/**
+ * Blocking SPI write/read.
+ *
+ * Sends byte, waits for received flag, then returns received byte.
+ */
+extern char SPI_sendrecv(char data);
+
+/**
+ * Restarts the SD card. Sets the block size to "block_size" bytes
+ */
+extern void SD_restart(char block_size);
+
+/**
+ * Write a null terminated buffer.
+ * the size of loc must be four bytes and loc must be big endian
+ */
+extern void SD_write(char *buff, char *loc, char n);
+
+/**
+ * Read n characters from SD
+ * the size of addr must be four bytes and should be big endian
+ */
+extern void SD_read(char *addr, char *buffer, char n);
+
+/**
+ * Sets the proper TRIS bits. Initializes master mode with clock idle high and sped = Fosc/4
+ */
+extern void SD_init(void);
+
+/**
+ * Restarts the SD card communication
+ */
+extern void SD_restart(char block_size);
