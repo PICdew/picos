705647ea62f033a5a1067f86a4fffd77deddf282
David Coss <david@davecoss.com>
2012-03-13 13:49:18 -0500

    Factored out linker code so that it will allow things that link against pasm_src.c to not need global code variables, such as "subroutines"

--- tools/pasm_src.c
+++ tools/pasm_src.c
@@ -25,15 +25,6 @@ int write_assembly(FILE *stream, const char *format, ...)
   return retval;
 }
 
-#if 0
-void _attach_label(struct compiled_code *ptrlist_end, picos_size_t label)
-{
-  if(ptrlist_end == NULL)
-    return;
-  ptrlist_end->label = label;
-}
-#endif
-
 void insert_compiled_code(nodeEnum type, struct compiled_code** ptrlist, struct compiled_code** ptrlist_end, picos_size_t val, picos_size_t label)
 {
   struct compiled_code *list = *ptrlist;
@@ -58,6 +49,36 @@ void insert_compiled_code(nodeEnum type, struct compiled_code** ptrlist, struct 
   (*ptrlist_end)->type = type;
 }
 
+
+/**
+ * Creates a stack, with the stack head pointer at the end.
+ * The size (stack_size) is the number STACK ELEMENTS. Thus,
+ * pstack will point to a linked list of stack_size+1 elements,
+ * where the last element is the stack head pointer.
+ * pstack_end will point to the last stack element, not the stack head.
+ * stack head is at (*pstack_end)->next
+ */
+void create_stack(struct compiled_code **pstack, struct compiled_code **pstack_end, size_t stack_size)
+{
+  struct compiled_code *stack, *end_of_stack;
+  if(pstack == NULL || pstack_end == NULL)
+    return;
+  stack = (struct compiled_code*)malloc(sizeof(struct compiled_code));
+  end_of_stack = stack;
+  stack->val = 0xff;
+
+  size_t stack_counter = 1;
+  for(;stack_counter < stack_size;stack_counter++)
+    {
+      end_of_stack->next = (struct compiled_code*)malloc(sizeof(struct compiled_code));
+      end_of_stack = end_of_stack->next;
+      end_of_stack->val = 0xff;
+    }
+  end_of_stack->next  = (struct compiled_code*)malloc(sizeof(struct compiled_code));// this is the stack head pointer.
+  
+  *pstack = stack;
+  *pstack_end = end_of_stack;
+}
 
 void FPrintCode(FILE *hex_file,struct compiled_code* code, int col, char *buffer,int start_address, int checksum, int print_type)
 {
@@ -109,139 +130,6 @@ void FPrintCode(FILE *hex_file,struct compiled_code* code, int col, char *buffer
   
   FPrintCode(hex_file,code->next,col,buffer,start_address,checksum,print_type);
 }
-
-
-const struct subroutine_map* lookup_subroutine(int index)
-{
-  extern struct subroutine_map *subroutines;
-  const struct subroutine_map *retval = subroutines;
-  while(retval != NULL)
-    {
-      if(retval->index == index)
-	break;
-      retval = retval->next;
-    }
-  
-  return retval;
-}
-
-int lookup_label(const struct compiled_code* code, picos_size_t label)
-{
-  int code_counter = 0;
-  if(code == NULL)
-    return -1;
-  for(;code != NULL;code = code->next)
-    {
-      if(code->type == typePCB)
-	continue;
-      if(code->type == typeLabel)
-	{
-	  if(code->label == label)
-	    return code_counter;
-	}
-      code_counter++;
-    }
-  return -1;
-}
-
-void resolve_labels(struct compiled_code* code)
-{
-  const struct compiled_code* code_head = code;
-  if(code == NULL)
-    return;
-  
-  for(;code != NULL;code = code->next)
-    {
-      switch(code->val)
-	{
-	case PICLANG_JMP:case PICLANG_JZ:case PICLANG_CALL:// change labels to addresses
-	  if(code->next != NULL)
-	    {
-	      int label_addr, subroutine_label;
-	      // Next next word should contain the target address.
-	      // 1.) If the type of the next word is a subroutine, the label
-	      //     of the subroutine must be found. That label is used
-	      //     to find the target *address*.
-	      // 2.) Otherwise the next byte is the label itself, which is
-	      //     used to find the target *address*.
-	      if(code->next->val == PASM_SUBROUTINE)
-		{
-		  const struct subroutine_map *subroutine = lookup_subroutine(code->next->label);
-		  if(subroutine == NULL || subroutine->label == -1)
-		    {
-		      if(subroutine == NULL)
-			fprintf(stderr,"Undefined reference: #%d\n",code->next->label);
-		      else
-			fprintf(stderr,"Undefined reference: %s\n",subroutine->name);
-		      exit(-1);
-		    }
-		  label_addr = lookup_label(code_head, subroutine->label);
-		}
-	      else
-		label_addr = lookup_label(code_head, code->next->label);
-	      if(label_addr < 0)
-		{
-		  fprintf(stderr,"Could not resolve label %d\n",code->next->label);
-		  return;
-		}
-	      code->next->val = (picos_size_t)label_addr;
-	      code = code->next;
-	      continue;
-	    }
-	  break;
-	case PICLANG_SIGNAL:
-	  if(code->next != NULL && code->next->next != NULL)
-	    {
-	      int label_addr = lookup_label(code_head, code->next->next->val);
-	      if(label_addr < 0)
-		{
-		  fprintf(stderr,"Could not resolve label %d\n",code->next->next->val);
-		  return;
-		}
-	      code->next->next->val = (picos_size_t)label_addr;
-	      code = code->next->next;
-	      continue;
-	    }
-	  break;
-	default:
-	  break;
-	}
-      if((opcode2assembly(code->val))->has_arg)
-	code = code->next;
-    }
-}
-
-/**
- * Creates a stack, with the stack head pointer at the end.
- * The size (stack_size) is the number STACK ELEMENTS. Thus,
- * pstack will point to a linked list of stack_size+1 elements,
- * where the last element is the stack head pointer.
- * pstack_end will point to the last stack element, not the stack head.
- * stack head is at (*pstack_end)->next
- */
-void create_stack(struct compiled_code **pstack, struct compiled_code **pstack_end, size_t stack_size)
-{
-  struct compiled_code *stack, *end_of_stack;
-  if(pstack == NULL || pstack_end == NULL)
-    return;
-  stack = (struct compiled_code*)malloc(sizeof(struct compiled_code));
-  end_of_stack = stack;
-  stack->val = 0xff;
-
-  size_t stack_counter = 1;
-  for(;stack_counter < stack_size;stack_counter++)
-    {
-      end_of_stack->next = (struct compiled_code*)malloc(sizeof(struct compiled_code));
-      end_of_stack = end_of_stack->next;
-      end_of_stack->val = 0xff;
-    }
-  end_of_stack->next  = (struct compiled_code*)malloc(sizeof(struct compiled_code));// this is the stack head pointer.
-  
-  *pstack = stack;
-  *pstack_end = end_of_stack;
-}
-
-
 
 void FreeCode(struct compiled_code* code)
 {
