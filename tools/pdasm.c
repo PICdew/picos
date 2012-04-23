51b8a388591d39b3b8d382a4a1d0dd6e9ca12c60
David Coss <david@davecoss.com>
2012-04-23 08:43:16 -0500

    Added ~/.picfsrc file usage to pdasm.

--- tools/pdasm.c
+++ tools/pdasm.c
@@ -8,6 +8,8 @@
 #include <getopt.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <unistd.h>
+
 
 picos_size_t FS_BUFFER_SIZE = 128;
 int label_counter = 0;
@@ -147,6 +149,117 @@ void dump_data(FILE *hex_file, FILE *assembly_file,PCB *pcb)
 }
 
 
+void load_rc(char *keyword, char *arg)
+{
+  size_t len,idx;
+  int have_arg, iarg;
+
+  if(keyword == NULL || strlen(keyword) == 0)
+    return;
+  
+  have_arg = (arg != NULL) & strlen(arg);
+
+  len = strlen(keyword);idx = 0;
+  for(;idx < len;idx++)
+    {
+      if(keyword[idx] == ' ')
+	{
+	  keyword[idx] = '\0';
+	  break;
+	}
+      keyword[idx] = tolower(keyword[idx]);
+    }
+  
+  if(have_arg)
+    {
+      len = strlen(arg);idx = 0;
+      for(;idx < len;idx++)
+	{
+	  if(arg[idx] == ' ')
+	    {
+	      arg[idx] = '\0';
+	      break;
+	    }
+	  arg[idx] = tolower(arg[idx]);
+	}
+    }
+
+  if(strcmp(keyword,"block_size") == 0)
+    {
+      if(have_arg)
+	{
+	  if(sscanf(arg,"%d",&iarg) != 1)
+	    fprintf(stderr,"Invalid block size \"%s\"\n",arg);
+	  else
+	    FS_BUFFER_SIZE = (picos_size_t)iarg;
+	}
+      else
+	fprintf(stderr,"No argument for BLOCK_SIZE\n");
+    }
+}
+
+
+void check_load_rc()
+{
+  char *home_dir = NULL;
+  const size_t buffer_size = 2048;
+  char rc_path[FILENAME_MAX], buf[buffer_size];
+  FILE *rc_file = NULL;
+  char *keyword, *arg, *saveptr;
+
+  home_dir = getenv("HOME");
+  
+  if(home_dir == NULL)
+    {
+      fprintf(stderr,"No home directory.\n");
+      return;
+    }
+  
+  sprintf(rc_path,"%s/.picfsrc",home_dir);
+
+  if(access(rc_path,R_OK) != 0)
+    {
+      // Cannot read rc file. Moving on...
+      return;
+    }
+  
+  rc_file = fopen(rc_path,"r");
+  
+  while(!feof(rc_file))
+    {
+      if(fgets(buf,buffer_size,rc_file) == NULL)
+	break;
+      arg = strrchr(buf,'\n');
+      if(arg)
+	*arg = '\0';
+      arg = strrchr(buf,'\r');
+      if(arg)
+	*arg = '\0';
+      arg = strchr(buf,'#');
+      if(arg)
+	*arg = '\0';
+      keyword = buf;
+      if(*keyword == 0)
+	continue;
+      while(*keyword == ' ')
+	keyword++;
+      if(strlen(keyword) == 0)
+	continue;
+      
+      arg = strchr(keyword,' ');
+      *arg = 0;arg++;
+      while(*arg == ' ')
+	arg++;
+      if(*arg == 0)
+	continue;
+      
+      load_rc(keyword,arg);
+    }
+  
+  fclose(rc_file);
+}
+
+
 static const char short_options[] = "a:b:h";
 enum OPTION_INDICES{OUTPUT_HEX};
 static struct option long_options[] =
@@ -179,6 +292,8 @@ int main(int argc, char **argv)
   int opt_index;
   picos_size_t block_size;
   PCB pcb;
+
+  check_load_rc();
   
   while(true)
     {    
