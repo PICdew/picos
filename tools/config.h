465e1f4c20b23a9283d295edd261924182cfcd61
David Coss <David.Coss@stjude.org>
2012-12-28 13:08:37 -0600

    Fixed PACKAGE_STRING in config.h.in.

--- tools/config.h
+++ tools/config.h
@@ -1,12 +1,12 @@
-#define PROJECT_NAME "PICOS"
-#define PACKAGE "PICOS"
-#define PACKAGE_NAME "PICOS"
+#define PROJECT_NAME "PICOS Tools"
+#define PACKAGE "PICOS Tools"
+#define PACKAGE_NAME "PICOS Tools"
 #define PACKAGE_BUGREPORT "david@davecoss.com"
-#define PACKAGE_STRING PACKAGE_NAME
 
 #define PICOS_MAJOR_VERSION 0
 #define PICOS_MINOR_VERSION 6
-#define VERSION 0.6
+#define VERSION "0.6"
+#define PACKAGE_STRING PACKAGE_NAME " " VERSION
 
 // Things for compilation for linux rather than uC
 #define bit int
